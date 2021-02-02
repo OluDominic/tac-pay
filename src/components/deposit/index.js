f05998acb6f17dac653b34489f5a4ef90cf1eb07
Dominic Oludare <dominicoludare@gmail.com>
2021-02-02 12:59:37 +0100

    update deposit

--- src/components/deposit/index.js
+++ src/components/deposit/index.js
@@ -1,32 +1,38 @@
 import React, {useState} from 'react'
 import Button from '../forms/Buttons';
 import Popup from './../popup'
-import { useAlert } from 'react-alert'
+import axios from 'axios'
 import { useHistory } from 'react-router-dom'
 import FormInput from '../forms/FornInput/formInput'
 import FormWrapper from '../formWrapper';
 import './index.scss'
 
 const Deposit =(props)=> {
-    const alert = useAlert()
     const history = useHistory();
-    const [amount, setAmount] = useState(" ");
+    const [amount, setAmount] = useState("");
     const [isOpen, setIsOpen] = useState(false);
+    const [date, setDate] = useState('')
+    const [comment, setComment] = useState("")
+    let [id, setID] = useState("")
 
     const handleSubmit =(event) => {
         event.preventDefault();
     }
 
+    const depositAPI =()=> {
+        axios.post("http://localhost:8000/deposit", {
+            id: id,
+            money: amount,
+            date: date,
+            comment: comment
+        }).then((response) => {
+            console.log(response)
+        })
+    }
+
     const togglePopup =()=> {
         setIsOpen(!isOpen);
     }
-
-    
-
-    const handleDeposit =()=> {
-        history.push('/profile')
-    }
-
     const configWrap = {
         head: 'Amount to deposit'
     }
@@ -37,12 +43,38 @@ const Deposit =(props)=> {
                 <h2>Deposit Page</h2>
                 <FormWrapper {...configWrap}>
                     <form onSubmit={handleSubmit}>
+
+                        <FormInput
+                        type="text"
+                        name="id"
+                        value={id}
+                        placeholder="ID"
+                        handleChange={e => setID(e.target.value) }
+                        />
+
                         <FormInput
                         type="text"
                         name="amount"
                         value={amount}
+                        placeholder="Amount"
                         handleChange={e => setAmount(e.target.value) }
                         />
+
+                        <FormInput
+                        type="text"
+                        name="date"
+                        value={date}
+                        placeholder="Date"
+                        handleChange={e => setDate(e.target.value) }
+                        />
+
+                        <FormInput
+                        type="text"
+                        name="comment"
+                        value={comment}
+                        placeholder="Comment"
+                        handleChange={e => setComment(e.target.value) }
+                        />      
 
                         <Button onClick={togglePopup} type="submit">
                             Deposit
@@ -52,7 +84,7 @@ const Deposit =(props)=> {
                          content={
                              <>
                                 <h3>Are you sure of the amount? N{amount}</h3>
-                                <Button onClick={handleDeposit}>Confirm Deposit</Button>
+                                <Button onClick={depositAPI}>Confirm Deposit</Button>
                              </>
                          }
                          handleClose={togglePopup}
